67fa0a063b0091f6337fc43786d16cd5ebd8a3c6
amos8426 <32868844+amos8426@users.noreply.github.com>
2018-01-12 18:04:12 +1100

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-'use strict';
+var mysql = require('mysql');
+var dboptions = { // database options
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
+};
+var db = mysql.createConnection(dboptions);// create mysql connection
+var express = require('express');//Importing Express
+var app = express();//Getting App From Express
+var fs = require('fs');//Importing File System Module To Access Files
+const port = process.env.PORT || 3000;//Creating A Constant For Providing The Port
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+//Routing Request : http://localhost:port/
+app.get('/',function(request,response){
+  //Telling Browser That The File Provided Is A HTML File
+  response.writeHead(200,{"Content-Type":"text/html"});
+  //Passing HTML To Browser
+  response.write(fs.readFileSync("./public/index.html"));
+  //Ending Response
+  response.end();
+})
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+//Routing To Public Folder For Any Static Context
+app.use(express.static(__dirname + '/public'));
+console.log("Server Running At:localhost:"+port);
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-console.log("the port is");
-console.log(PORT);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-      socket.on('clicked', function() {
-        io.emit('clicked');
-    });
-  socket.on('disconnect', () => console.log('Client disconnected'));
+//confirm mysql database connection
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to test database!");
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+//run socket.io code
+var io = require('socket.io').listen(app.listen(port));//Telling Express+Socket.io App To Listen To Port
+io.sockets.on("connection",function(socket){
+    socket.emit("Start_Chat");
+})
